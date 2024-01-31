fa9208dde398f0ade179af7ca747676ffc76b311
Noelia Estela Cuenca <48950281+noeliaescu@users.noreply.github.com>
2024-01-31 18:38:39 +0100

    Create type-search-index.js

---
+++ Afinidades_TrabajoEntorno/doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"MiProgramaMain"},{"p":"<Unnamed>","l":"Persona"},{"p":"<Unnamed>","l":"TestPrograma"}];updateSearchResults();
